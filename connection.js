98b26f03f3e95b262f721a58e197b1a1b504890d
Precious Affiah <preciousaffiah205@gmail.com>
2022-11-01 02:17:38 +0100

    update env

--- connection.js
+++ connection.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const user = process.env.MONGO_USER;
 const password = process.env.MONGO_PASSWORD;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const connectionParams = {
     useNewUrlParser: true, 
@@ -11,7 +12,7 @@ const connectionParams = {
 }
 
 // const uri = `mongodb+srv://${user}:${password}@cluster0.di9pr.mongodb.net/?retryWrites=true&w=majority`
-const connection = mongoose.connect(process.env.MONGODB_URI, connectionParams).then(()=>console.log('connected to cloud atlas'))
+const connection = mongoose.connect(MONGODB_URI, connectionParams).then(()=>console.log('connected to cloud atlas'))
 .catch((err)=>console.log(err));
 
 module.exports = connection  
